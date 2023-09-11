5e1d844a3af5860bb38d99a2cdcf03c13737d8ab
JuanVictorf <estudantejuan0@gmail.com>
2023-09-11 20:05:17 -0300

    Criando serviço

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -7,13 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  name:string = "BULBASSAUR";
-  attributesTypes:string[] = ["FIRE", "ROCK"]
+  pokemon:PokemonData;
 
-
-  constructor() { }
+  constructor(private service:PokemonService) { 
+    this.pokemon = {
+      id: 0,
+      name: "",
+      sprites: {
+        front_default: "",
+      },
+      types: []
+    }
+    
+  }
 
   ngOnInit(): void {
+    this.getPokemon("pikachu");
+  }
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+          console.log(res)
+          console.log(this.pokemon)
+          
+        }
+      }
+    )
   }
 
 }
